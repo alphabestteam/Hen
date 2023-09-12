d1e8c0d444ea978166d3695aa91994149df9cecd
a0526604945@gmail.com <hen>
2023-09-12 10:06:27 +0300

    finish pdf2

---
+++ main_person2.py
@@ -0,0 +1,27 @@
+from person import person
+
+def main():
+    p1 = person()
+    p2 = person()
+    p3 = person()
+    p4 = person()
+    p5 = person()
+    p6 = person()
+    p7 = person()
+    p8 = person()
+    p9 = person()
+    p10 = person()
+    print(p1)
+    print(p2)
+    print(p3)
+    print(p4)
+    print(p5)
+    print(p6)
+    print(p7)
+    print(p8)
+    print(p9)
+    print(p10)
+
+
+if __name__ == "__main__":
+    main()
