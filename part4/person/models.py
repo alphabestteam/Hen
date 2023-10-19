3b0e19d3af1a2795c64d984b14a551c7fb1ab3a8
a0526604945@gmail.com <hen>
2023-10-19 10:41:14 +0300

    finish part5

--- part4/person/models.py
+++ part4/person/models.py
@@ -6,3 +6,8 @@ class Person(models.Model):
     id = models.IntegerField(primary_key=True)
     birth_data = models.DateField()
     city = models.CharField(max_length=20)
+
+class Parent(Person):
+    work_place = models.CharField(max_length=30)
+    salary = models.DecimalField(max_digits=8,decimal_places=2)
+    kids = models.ManyToManyField(Person,related_name="parents", default=[])
--- part4/person/urls.py
+++ part4/person/urls.py
@@ -2,8 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/getAllPeople/',views.all_persons,name='all_persons'),
-    path('api/addPerson/',views.add_person,name='add_person'),
-    path('api/updatePerson/',views.update_person,name='update_person'),
-    path('api/removePerson/<int:person_id>/',views.remove_person,name='remove_person'),
+    path('getAllPeople/',views.all_persons,name='all_persons'),
+    path('addPerson/',views.add_person,name='add_person'),
+    path('updatePerson/',views.update_person,name='update_person'),
+    path('removePerson/<int:person_id>/',views.remove_person,name='remove_person'),
+
+
+    path('addParent/',views.add_parent,name='add_parent'),
+    path('getAllParents/',views.all_parents,name='all_parents'),
+    path('removeParent/<int:parent_id>/',views.remove_parent,name='remove_parent'),
+    path('updateParent/',views.update_parent,name='update_parent'),
+    path('addKid/<int:parent_id>/<int:kid_id>/',views.add_kid,name='add_kid'),
+    path('getParentAndKid/<int:parent_id>/',views.get_parent_and_kid,name='get_parent_and_kid'),
+    path('richKid/',views.rich_kid,name='rich_kid'),
+    path('findParents/<int:kid_id>/',views.find_parents,name='find_parents'),
+    path('findKids/<int:parent_id>/',views.find_kids,name='find_kids'),
+    path('findGrand/<int:grand_kid_id>/',views.find_grand,name='find_grand'),
+    path('findBrothers/<int:brother_id>/',views.find_brothers,name='find_brothers')
 ]
