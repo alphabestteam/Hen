9368bb24d2fd8ac4a66d4f7052d23d2cc5592dd0
a0526604945@gmail.com <hen>
2023-11-01 16:29:09 +0200

    finish pdf7

---
+++ FriendList.js
@@ -0,0 +1,11 @@
+const friendList = ["sandy","patrick","squidward","Mr. krabs","Gary"];
+console.log(friendList);
+console.log(friendList.length);
+friendList[friendList.length] = "hen";
+console.log(friendList);
+console.log(friendList.length);
+friendList[0] = "yair";
+console.log(friendList)
+
+// when i write const on array it just tell the program that i cant change from array to number or string 
+// but the contents of the arrray i can steal change
