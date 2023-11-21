fe3fe44f08576be09114954570b783b4df4f829a
a0526604945@gmail.com <hen>
2023-11-21 18:52:33 +0200

    finish part 6

---
+++ ex6/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  myForm!: FormGroup;
+  formSubmitted = false;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  private buildForm() {
+    this.myForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    this.formSubmitted = true; 
+  }
+}
+
