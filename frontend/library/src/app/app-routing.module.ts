ba851eadc98561c46a0a27c6e335cea0b05f22c4
a0526604945@gmail.com <hen>
2023-12-24 16:29:35 +0200

    make gaurds and more css

--- frontend/library/src/app/app-routing.module.ts
+++ frontend/library/src/app/app-routing.module.ts
@@ -8,15 +8,16 @@ import { PersonalAreaComponent } from './personal-area/personal-area.component';
 import { UploadBooksComponent } from './upload-books/upload-books.component';
 import { UpdateBookComponent } from './update-book/update-book.component';
 import { PostReviewComponent } from './post-review/post-review.component';
+import { authGuard } from './auth.guard';
 
 
 const routes: Routes = [
-  { path: 'post-review/:bookId/:bookType', component:PostReviewComponent},
-  { path: 'update-books/:id', component:UpdateBookComponent},
-  { path: 'upload-books', component:UploadBooksComponent},
-  { path: 'personal-area', component:PersonalAreaComponent},
-  { path: 'book-details/:id', component: BookDetailsComponent },
-  { path: 'home', component: HomeComponent},
+  { path: 'post-review/:bookId/:bookType', component:PostReviewComponent, canActivate:[authGuard]},
+  { path: 'update-books/:id', component:UpdateBookComponent, canActivate:[authGuard]},
+  { path: 'upload-books', component:UploadBooksComponent, canActivate:[authGuard]},
+  { path: 'personal-area', component:PersonalAreaComponent, canActivate:[authGuard]},
+  { path: 'book-details/:id', component: BookDetailsComponent, canActivate:[authGuard] },
+  { path: 'home', component: HomeComponent , canActivate:[authGuard]},
   { path: 'login', component: LogPageComponent },
   { path: 'signup', component: SingUpComponent },
   { path: '**', redirectTo: 'login', pathMatch: 'full' },
---
+++ frontend/library/src/app/auth.guard.ts
@@ -0,0 +1,19 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+
+export const authGuard: CanActivateFn = (route, state) => {
+  
+  const session = sessionStorage.getItem('id');
+  console.log(session);
+  
+  const router = inject(Router)
+
+  
+  if (!session)  {
+    router.navigate(['login'])
+    return false
+  } else {
+    return true
+  }
+ };
